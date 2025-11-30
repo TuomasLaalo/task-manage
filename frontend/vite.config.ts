f3351c58c6ca8f46e806e029fc21bee2f0ae7bcd
Tuomas Laalo <bhm500@myy-haaga-helia.fi>
2025-11-30 22:42:18 +0200

    1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,24 +1,10 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
   }
-})
+});
